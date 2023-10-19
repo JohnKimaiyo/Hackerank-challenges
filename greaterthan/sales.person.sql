b0941cb867d038741e4fdd3b6d2f084ae69c5bed
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-10-19 08:34:33 +0300

    final commit

---
+++ greaterthan/sales.person.sql
@@ -0,0 +1,12 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT  [BusinessEntityID]
+      ,[TerritoryID]
+      ,[SalesQuota]
+      ,[Bonus]
+      ,[CommissionPct]
+      ,[SalesYTD]
+      ,[SalesLastYear]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Sales].[SalesPerson]
+  WHERE Bonus >=3000;
