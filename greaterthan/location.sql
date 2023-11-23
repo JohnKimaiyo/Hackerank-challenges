fd95bfcaf1a54cdf4eebcf9fc21fea83a8af7926
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-23 12:31:17 +0300

    final commit

--- greaterthan/location.sql
+++ greaterthan/location.sql
@@ -15,3 +15,18 @@ SELECT TOP (1000) [CurrencyRateID]
       ,[ModifiedDate]
   FROM [AdventureWorks2019].[Sales].[CurrencyRate]
   WHERE AverageRate > 10 AND EndOfDayRate > 1;
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [SalesOrderID]
+      ,[SalesOrderDetailID]
+      ,[CarrierTrackingNumber]
+      ,[OrderQty]
+      ,[ProductID]
+      ,[SpecialOfferID]
+      ,[UnitPrice]
+      ,[UnitPriceDiscount]
+      ,[LineTotal]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Sales].[SalesOrderDetail]
+  WHERE OrderQty > 3 AND LineTotal > 2000;
