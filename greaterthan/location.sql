8de2e5a5859c4dbe5d8444512d2d3c0ac811fd36
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-23 12:26:58 +0300

    final commit

--- greaterthan/location.sql
+++ greaterthan/location.sql
@@ -4,3 +4,14 @@ SELECT
      
   FROM [AdventureWorks2019].[Production].[Location]
   WHERE CostRate >=10;
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [CurrencyRateID]
+      ,[CurrencyRateDate]
+      ,[FromCurrencyCode]
+      ,[ToCurrencyCode]
+      ,[AverageRate]
+      ,[EndOfDayRate]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Sales].[CurrencyRate]
+  WHERE AverageRate > 10 AND EndOfDayRate > 1;
