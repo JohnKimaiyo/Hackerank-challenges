71cbe5c26218afe22fc7b8ae9502f2196c7d1e85
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-26 19:15:18 +0300

    final commit

---
+++ Equals/equals.sql
@@ -0,0 +1,95 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [CountryRegionCode]
+      ,[Name]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Person].[CountryRegion]
+  WHERE CountryRegionCode = 'AM'
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [BusinessEntityID]
+      ,[PersonType]
+      ,[NameStyle]
+      ,[Title]
+      ,[FirstName]
+      ,[MiddleName]
+      ,[LastName]
+      ,[Suffix]
+      ,[EmailPromotion]
+      ,[AdditionalContactInfo]
+      ,[Demographics]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Person].[Person]
+  WHERE MiddleName = 'J'
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [StateProvinceID]
+      ,[StateProvinceCode]
+      ,[CountryRegionCode]
+      ,[IsOnlyStateProvinceFlag]
+      ,[Name]
+      ,[TerritoryID]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Person].[StateProvince]
+  WHERE CountryRegionCode = 'CA'
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [TransactionID]
+      ,[ProductID]
+      ,[ReferenceOrderID]
+      ,[ReferenceOrderLineID]
+      ,[TransactionDate]
+      ,[TransactionType]
+      ,[Quantity]
+      ,[ActualCost]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Production].[TransactionHistory]
+  WHERE TransactionType = 'W';
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [ProductID]
+      ,[BusinessEntityID]
+      ,[AverageLeadTime]
+      ,[StandardPrice]
+      ,[LastReceiptCost]
+      ,[LastReceiptDate]
+      ,[MinOrderQty]
+      ,[MaxOrderQty]
+      ,[OnOrderQty]
+      ,[UnitMeasureCode]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Purchasing].[ProductVendor]
+  WHERE UnitMeasureCode = 'CS';
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [CountryRegionCode]
+      ,[CurrencyCode]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Sales].[CountryRegionCurrency]
+  WHERE CurrencyCode = 'EUR';
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [CreditCardID]
+      ,[CardType]
+      ,[CardNumber]
+      ,[ExpMonth]
+      ,[ExpYear]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Sales].[CreditCard]
+  WHERE  CardType = 'Vista';
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [SpecialOfferID]
+      ,[Description]
+      ,[DiscountPct]
+      ,[Type]
+      ,[Category]
+      ,[StartDate]
+      ,[EndDate]
+      ,[MinQty]
+      ,[MaxQty]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Sales].[SpecialOffer]
+  WHERE Category = 'Reseller';
