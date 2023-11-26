d448ac48c981144cbff3172278e8d49861591060
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-26 18:58:48 +0300

    final comit

--- Equals/equals.sql
+++ Equals/equals.sql
@@ -4,3 +4,20 @@ SELECT TOP (1000) [CountryRegionCode]
       ,[ModifiedDate]
   FROM [AdventureWorks2019].[Person].[CountryRegion]
   WHERE CountryRegionCode = 'AM'
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [BusinessEntityID]
+      ,[PersonType]
+      ,[NameStyle]
+      ,[Title]
+      ,[FirstName]
+      ,[MiddleName]
+      ,[LastName]
+      ,[Suffix]
+      ,[EmailPromotion]
+      ,[AdditionalContactInfo]
+      ,[Demographics]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[Person].[Person]
+  WHERE MiddleName = 'J'
