8cec05030ae7bf5eeeb5ae00a7b6014a9c1d7bdb
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-26 19:29:32 +0300

    final commit

--- Round and Sum/round and sum.sql
+++ Round and Sum/round and sum.sql
@@ -3,3 +3,11 @@ SELECT
       ROUND(SUM([Rate]),2)
      
   FROM [AdventureWorks2019].[HumanResources].[EmployeePayHistory]
+
+
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT  ROUND(SUM(
+      [CostRate]),2) AS TotalCostRate,ROUND(SUM([Availability]  ),2  ) AS TotalAvailability
+      
+      
+  FROM [AdventureWorks2019].[Production].[Location]
